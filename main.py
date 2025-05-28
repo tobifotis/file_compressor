411f3b5f0e116d558b892d2aac784bfda3d4f7e0
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-05-28 07:26:15 -0500

    feat: add Compress button and complete initial GUI layout

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ label2 = sg.Text("Select destination folder:")
 input2 = sg.Input()
 choose_button2 = sg.FolderBrowse("Choose")
 
+compress_button = sg.Button("Compress")
+
+window = sg.Window("File Compressor",
+                   layout=[[label1, input1, choose_button1],
+                           [label2, input2, choose_button2],
+                           [compress_button]])
+
+window.read()
+window.close()
